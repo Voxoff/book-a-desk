96d64d22e5e646cebd26f80ed43834422f7fbf27
guy <guy>
2019-03-29 17:06:37 +0000

    added proxy, react router and two blank componenets

---
+++ src/components/TableList.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class TableList extends Component {
+  render() {
+    return (
+      <div className="TableList">
+
+      </div>
+    );
+  }
+}
+
+export default TableList;
