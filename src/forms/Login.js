b276ed127079490125a607c291b04d8e047952c9
guy <guy>
2019-04-10 21:29:43 +0100

    oopsie

--- src/forms/Login.js
+++ src/forms/Login.js
@@ -34,7 +34,7 @@ class Login extends Component {
           appId="19afa3f5fa107ddca86a"
           gatekeeper="https://flatiron-gate.herokuapp.com/"
           onLoginSuccess={this.handleSocialLogin}
-          redirect="https://book-a-desk-api.herokuapp.com"
+          redirect="https://book-a-desk.herokuapp.com"
           onLoginFailure={this.handleSocialLoginFailure}
         >
           Login with Github
