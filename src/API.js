9cfa306ce054345b652ea6e101f346053dbd1737
Guy Bennett-Jones <Voxoff>
2019-04-23 22:01:40 +0100

    corrected redirect

--- src/API.js
+++ src/API.js
@@ -1,9 +1,11 @@
 class API {
   static init() {
     this.gatekeeperURL = 'https://flatiron-keeper-of-gates.herokuapp.com'
+    this.baseURL = 'https://book-a-desk-api.herokuapp.com/api/v1'
+    this.reactURL = 'https://book-a-desk.herokuapp.com'
     // this.gatekeeperURL = 'http://localhost:9999'
     // this.baseURL = 'http://localhost:3000/api/v1'
-    this.baseURL = 'https://book-a-desk-api.herokuapp.com/api/v1'
+    // this.reactURL = 'http://localhost:3000'
     this.addBookingURL = this.baseURL + '/add_booking'
     this.tablesURL = this.baseURL + '/tables'
     this.callbackURL = this.baseURL + '/auth/github/callback'
