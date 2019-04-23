f772e5a94b9bdb1d518d55ec93f7369fd518983a
Guy Bennett-Jones <Voxoff>
2019-04-23 21:56:21 +0100

    typo in URL

--- src/API.js
+++ src/API.js
@@ -3,7 +3,7 @@ class API {
     this.gatekeeperURL = 'https://flatiron-keeper-of-gates.herokuapp.com'
     // this.gatekeeperURL = 'http://localhost:9999'
     // this.baseURL = 'http://localhost:3000/api/v1'
-    this.baseURL = 'https://book-a-desk-app.herokuapp.com/api/v1'
+    this.baseURL = 'https://book-a-desk-api.herokuapp.com/api/v1'
     this.addBookingURL = this.baseURL + '/add_booking'
     this.tablesURL = this.baseURL + '/tables'
     this.callbackURL = this.baseURL + '/auth/github/callback'
